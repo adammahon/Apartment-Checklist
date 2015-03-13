93f24d3c4c4911fad8f5e63a56110c5f076f3367
Adam Mahon <amahon.1123@gmail.com>
2015-03-12 21:22:42 -0400

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,38 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('apartmentChecklist',
+    ['ionic',
+        'apartmentChecklist.controllers'])
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('index', {
+                url: '/',
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .state('apartment', {
+                url: '/apartment',
+                templateUrl: 'views/apartment.html',
+                controller: 'ApartmentCtrl'
+            });
+
+        $urlRouterProvider.otherwise('/');
+    });
---
+++ www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('apartmentChecklist.controllers', [])
+    .controller('MainCtrl', function ($scope) {
+
+    })
+    .controller('ApartmentCtrl', function ($scope) {
+
+    });
